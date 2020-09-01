f978e762b6cbe2a84dd696bf346ee524ba30b381
Joshua Riley <Joshy@Gilly-in-Philly.localdomain>
2020-09-01 12:40:07 -0700

    first commit

---
+++ function_tutorial.py
@@ -0,0 +1,33 @@
+def area(width, height):
+    result = width * height
+    return result
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+def add(num1,num2):
+    result = num1 + num2
+    return result
+
+result = area(5, 6)
+print(result)
+
+result = area(15, 6)
+print(result)
+
+result = area(30, 6)
+print(result)
+
+result = subtract(5,6)
+print(result)
+
+result = add(5, 6)
+print(result)
+
+result = add(10, 6)
+print(result)
